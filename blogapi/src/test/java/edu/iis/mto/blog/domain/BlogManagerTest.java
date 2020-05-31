9b30a8c24320947b4cb9bc666d15d57c29d475d7
Przemysław Mirowski <miretpl@gmail.com>
2020-06-01 01:00:02 +0200

    add tests of addLikeToPost method

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,9 +1,17 @@
 package edu.iis.mto.blog.domain;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -20,9 +28,17 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
+
+    @MockBean
+    private BlogPostRepository blogPostRepository;
+
+    @MockBean
+    private LikePostRepository likePostRepository;
 
     @MockBean
     private UserRepository userRepository;
@@ -36,6 +52,30 @@ public class BlogManagerTest {
     @Captor
     private ArgumentCaptor<User> userParam;
 
+    private User user;
+    private BlogPost blogPost;
+
+    @Before
+    public void setUp() throws Exception {
+        Long userID = 1L;
+        Long ownerID = 2L;
+        Long blogPostID = 3L;
+
+        User owner = new User();
+        owner.setId(ownerID);
+
+        user = new User();
+        user.setId(userID);
+
+        blogPost = new BlogPost();
+        blogPost.setId(blogPostID);
+        blogPost.setUser(owner);
+
+        when(userRepository.findById(ownerID)).thenReturn(Optional.of(owner));
+        when(userRepository.findById(userID)).thenReturn(Optional.of(user));
+        when(blogPostRepository.findById(blogPostID)).thenReturn(Optional.of(blogPost));
+    }
+
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
@@ -44,4 +84,21 @@ public class BlogManagerTest {
         assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
 
+    @Test
+    public void shouldDoesNotThrowDomainErrorWithUserAccountStatusEqualsConfirmed() {
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+        assertDoesNotThrow(() -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
+    }
+
+    @Test
+    public void shouldThrowDomainErrorWithUserAccountStatusEqualsNew() {
+        user.setAccountStatus(AccountStatus.NEW);
+        assertThrows(DomainError.class, () -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
+    }
+
+    @Test
+    public void shouldThrowDomainErrorWithUserAccountStatusEqualsRemoved() {
+        user.setAccountStatus(AccountStatus.REMOVED);
+        assertThrows(DomainError.class, () -> blogService.addLikeToPost(user.getId(), blogPost.getId()));
+    }
 }
