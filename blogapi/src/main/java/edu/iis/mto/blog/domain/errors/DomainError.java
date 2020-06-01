e9625d12aa4d919f17559a8ffd31dfe929ef4396
Przemysław Mirowski <miretpl@gmail.com>
2020-06-01 18:06:41 +0200

    add USER_REMOVED to DomainError and modify BlogDataFinder

--- blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/errors/DomainError.java
@@ -6,6 +6,7 @@ public class DomainError extends RuntimeException {
 
     public static final String USER_NOT_CONFIRMED = "unknown user";
     public static final String USER_NOT_FOUND = "unknown user";
+    public static final String USER_REMOVED = "user removed";
     public static final String POST_NOT_FOUND = "unknown post";
     public static final String SELF_LIKE = "cannot like own post";
 
