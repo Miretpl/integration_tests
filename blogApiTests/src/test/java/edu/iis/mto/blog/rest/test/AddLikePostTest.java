1f08720db46c04b2952616018f2741beddaa197b
Przemysław Mirowski <miretpl@gmail.com>
2020-06-01 14:59:58 +0200

    add AddLikePostTest class

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AddLikePostTest.java
@@ -0,0 +1,176 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class AddLikePostTest extends FunctionalTests {
+
+    @Test
+    public void addLikeToBlogPostWithoutLikesByOwnerOfPostShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_OWNER_TO_POST_WITHOUT_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithLikesByOwnerOfPostShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_OWNER_TO_POST_WITH_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithoutLikesByUserWithConfirmAccountStatusShouldReturnOkStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITHOUT_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithLikesByUserWithConfirmAccountStatusShouldReturnOkStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITH_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithoutLikesByUserWithNewAccountStatusShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_NEW_USER_TO_POST_WITHOUT_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithLikesByUserWithNewAccountStatusShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_NEW_USER_TO_POST_WITH_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithoutLikesByUserWithRemoveAccountStatusShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_REMOVE_USER_TO_POST_WITHOUT_LIKES);
+    }
+
+    @Test
+    public void addLikeToBlogPostWithLikesByUserWithRemoveAccountStatusShouldReturnBadRequestStatus() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_BAD_REQUEST)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_REMOVE_USER_TO_POST_WITH_LIKES);
+    }
+
+    @Test
+    public void addTwoLikesToBlogPostWithoutLikesByUserShouldNotChangeLikesCountOfBlogPost() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITHOUT_LIKES);
+
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITHOUT_LIKES);
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(StaticElements.GET_BLOG_POST_WITHOUT_LIKES_INFO)
+                .then()
+                .body("likesCount", is(equalTo(1)));
+    }
+
+    @Test
+    public void addTwoLikesToBlogPostWithLikesByUserShouldNotChangeLikesCountOfBlogPost() {
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITH_LIKES);
+
+        given().accept(ContentType.JSON)
+               .header("Content-Type", "application/json;charset=UTF-8")
+               .expect()
+               .log()
+               .all()
+               .statusCode(HttpStatus.SC_OK)
+               .when()
+               .post(StaticElements.ADD_LIKE_BY_CONFIRM_USER_TO_POST_WITH_LIKES);
+
+        given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .get(StaticElements.GET_BLOG_POST_WITH_LIKES_INFO)
+                .then()
+                .body("likesCount", is(equalTo(2)));
+    }
+
+}
